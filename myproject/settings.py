d4129f7ba0f4dd4bc95a61b86a81b8ee7d760f63
Animesh Tirtha <45541708+animeshdashtirtha@users.noreply.github.com>
2025-09-09 23:52:12 +0600

    supabase

--- myproject/settings.py
+++ myproject/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 import os
+from decouple import config
 
 NPM_BIN_PATH = r"C:\Program Files\nodejs\npm.cmd"
 
@@ -23,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-37#jgp_16notie7qwic2+nb*-v0ar)q#k3b&$&ea5p5uopc0vc'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -96,11 +97,11 @@ WSGI_APPLICATION = 'myproject.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',  # Use PostgreSQL engine
-        'NAME': 'myproject',               # Your database name
-        'USER': 'postgres',               # Your PostgreSQL username
-        'PASSWORD': 'Tirtha07',       # Your PostgreSQL password
-        'HOST': 'localhost',                        # Set to your PostgreSQL host (usually localhost)
-        'PORT': '5432',                             # Default PostgreSQL port
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD'),
+        'HOST': config('DB_HOST'),
+        'PORT': config('DB_PORT', cast=int)   # Default PostgreSQL port
     }
 }
 
